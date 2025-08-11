20d2fce458b29c86e3d48d4518f97da606b96763
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-08-11 12:46:23 +0400

    tokenizer

--- my_models/Transformer/tokenization.py
+++ my_models/Transformer/tokenization.py
@@ -2,9 +2,7 @@ import tiktoken
 
 
 class TikTokenizer:
-    """
-        Токенизатор, основанный на токенизаторе openai
-    """
+    """Токенизатор, основанный на токенизаторе openai"""
     def __init__(self, model_name="gpt-4"):
         self.enc = tiktoken.encoding_for_model(model_name)
 
@@ -30,11 +28,11 @@ class TikTokenizer:
         self.vocab_size = max(self._idx2token.keys()) + 1
 
     def encode(self, text):
-        """Преобразование текста в числовые токены"""
+        """Преобразование текста в токены-индексы"""
         return self.enc.encode(text)
 
     def decode(self, ids):
-        """Преобразование числовых токенов в текст"""
+        """Преобразование токенов по индексу в текст"""
         tokens = []
         for token_id in ids:
             if token_id in self._idx2token:
@@ -44,21 +42,11 @@ class TikTokenizer:
                 tokens.append(self.enc.decode([token_id]))
         return "".join(tokens)
 
-    def convert_token_to_id(self, token):
-        """Преобразование токена в число"""
+    def token2idx(self, token):
+        """Преобразование токена в индекс"""
         return self.token2idx.get(token, self.token2idx["<unk>"])
 
-    def convert_id_to_token(self, token_id):
+    def idx2token(self, token_id):
         """Получение токена по его индексу"""
         return self._idx2token.get(token_id, self._idx2token[0])
-
-
-tokenizer = TikTokenizer()
-print("Vocab size:", tokenizer.vocab_size)
-print("Decode <unk>:", tokenizer.decode([0]))
-print("Decode 12561:", tokenizer.decode([12561]))
-print("Decode 28089:", tokenizer.decode([28089]))
-print("Decode 8341:", tokenizer.decode([8341]))
-print("Encode Russian:", tokenizer.encode("Hello, привет, как дела"))
-print("Decode Russian:", tokenizer.decode(tokenizer.encode("Hello, привет, как дела djhf авол")))
 
