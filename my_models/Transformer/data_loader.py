e07b3c1eff3c3e21ca74373c1c33709e12e858c7
Veronika_Steklo <151031140+VeronikaSteklo@users.noreply.github.com>
2025-08-27 13:26:43 +0400

    add clean text for data

--- my_models/Transformer/data_loader.py
+++ my_models/Transformer/data_loader.py
@@ -1,16 +1,34 @@
+import re
+
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
 
+def clean_text(text: str) -> str:
+    """Очистка текста: нижний регистр + удаление пунктуации кроме тире (-) и двоеточия (:)"""
+    text = text.lower()
+    text = re.sub(r"[^а-яa-z0-9:\-\s]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
+
+
 class TextTitleDataset(Dataset):
-    def __init__(self, data_path, text_fill="text", title_fill="title", tokenizer=None, max_len=512):
-        self.data = pd.read_csv(data_path).head(10000)
+    def __init__(
+            self, data_path, text_fill="text",
+            title_fill="title", tokenizer=None, max_text_len=512,
+            max_title_len=25, use_clean=True, limit=None
+    ):
+        self.data = pd.read_csv(data_path)
+        if limit:
+            self.data = self.data.head(limit)
         print(f"Количество данных в исходном датасете: {len(self.data)}")
         self.text_fill = text_fill
         self.title_fill = title_fill
         self.tokenizer = tokenizer
-        self.max_len = max_len
+        self.max_text_len = max_text_len
+        self.max_title_len = max_title_len
+        self.use_clean = use_clean
 
     def __len__(self):
         return len(self.data)
@@ -21,11 +39,15 @@ class TextTitleDataset(Dataset):
         text = str(row[self.text_fill])
         title = str(row[self.title_fill])
 
+        if self.use_clean:
+            text = clean_text(text)
+            title = clean_text(title)
+
         text_tokens = self.tokenizer.encode(text, add_special_tokens=False)
         title_tokens = self.tokenizer.encode(title, add_special_tokens=True)
 
-        text_tokens = text_tokens[:self.max_len]
-        title_tokens = title_tokens[:self.max_len]
+        text_tokens = text_tokens[:self.max_text_len]
+        title_tokens = title_tokens[:self.max_title_len]
 
         return {
             "src": torch.tensor(text_tokens, dtype=torch.long),
